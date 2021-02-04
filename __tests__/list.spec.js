a6178c7d1155e3050b4c4e3a498f3fd80884036b
Filipoliko <satek.filip@gmail.com>
2021-02-04 18:40:34 +0100

    Extend test suites with more error handling scenarios; Add eslint for strict codestyle

--- __tests__/list.spec.js
+++ __tests__/list.spec.js
@@ -1,160 +1,313 @@
-import frisby, { Joi } from 'frisby';
-import Auth from '../fixtures/auth';
-import { apiUrl } from '../config';
+import frisby, { Joi } from "frisby";
+import Auth from "../fixtures/auth";
+import { apiUrl } from "../config";
 
-describe('List', () => {
-    describe('with read-only access', () => {
-        beforeAll(() => {
-            return Auth.setReadAccess();
+describe("List (read-only access)", () => {
+  beforeAll(() => {
+    return Auth.setReadAccess();
+  });
+
+  it("cannot be created", () => {
+    return frisby
+      .post("https://api.themoviedb.org/4/list", {
+        name: "My Cool List",
+        iso_639_1: "en",
+      })
+      .expect("status", 401);
+  });
+});
+
+describe("List (write access)", () => {
+  let listId;
+
+  beforeAll(() => {
+    return Auth.setWriteAccess();
+  });
+
+  afterAll(async () => {
+    // Delete the created list to cleanup after test run
+    await frisby
+      .delete(`${apiUrl}/list/${listId}`)
+      .expect("status", 200)
+      .expect("json", { success: true });
+
+    // Check, that list is actually deleted
+    await frisby.get(`${apiUrl}/list/${listId}`).expect("status", 404);
+  });
+
+  it("cannot be created without required fields", async () => {
+    await frisby
+      .post(`${apiUrl}/list`, {})
+      .expect("status", 422)
+      .expect("fieldsRequired", ["name", "iso_639_1"]);
+  });
+
+  it("cannot be created with invalid field values", async () => {
+    await frisby
+      .post(`${apiUrl}/list`, {
+        name: 1,
+        iso_639_1: 1,
+        description: 1,
+        public: 1,
+      })
+      // @FIXME API should not return internal error, once
+      // fixed on API side, we should update this assertion
+      // to check the validation error message
+      .expect("status", 500)
+      .expect("json", { success: false });
+  });
+
+  it("can be created with required fields only", async () => {
+    const postValues = {
+      name: "My Cool List",
+      iso_639_1: "en",
+    };
+    const response = await frisby
+      .post(`${apiUrl}/list`, postValues)
+      .expect("status", 201)
+      .expect("jsonTypes", { id: Joi.number().required() })
+      .expect("json", { success: true });
+
+    // Save the list ID to be used in other test cases
+    listId = response.json.id;
+
+    // Check the list existance
+    await frisby
+      .get(`${apiUrl}/list/${listId}`)
+      .expect("status", 200)
+      .expect("json", postValues);
+  });
+
+  it("cannot be updated with invalid values", async () => {
+    await frisby
+      .put(`${apiUrl}/list/${listId}`, {
+        name: 1,
+        iso_639_1: 1,
+        description: 1,
+        public: 1,
+      })
+      // @FIXME API should not return internal error, once
+      // fixed on API side, we should update this assertion
+      // to check the validation error message
+      .expect("status", 500)
+      .expect("json", { success: false });
+  });
+
+  it("can be updated", async () => {
+    const updateValues = {
+      name: "My Cool Updated List",
+      description: "Cool Description",
+      public: true,
+    };
+
+    // Update the list
+    await frisby
+      .put(`${apiUrl}/list/${listId}`, updateValues)
+      .expect("status", 201)
+      .expect("json", { success: true });
+
+    // Check the updated fields in the list
+    await frisby
+      .get(`${apiUrl}/list/${listId}`)
+      .expect("status", 200)
+      .expect("json", updateValues);
+  });
+
+  describe("items", () => {
+    const items = [
+      {
+        media_id: 652,
+        media_type: "movie",
+      },
+      {
+        media_id: 60573,
+        media_type: "tv",
+      },
+    ];
+
+    it("cannot be added without required fields", async () => {
+      await frisby
+        .post(`${apiUrl}/list/${listId}/items`, {
+          items: [
+            {
+              // Empty item
+            },
+          ],
+        })
+        // @FIXME Status code 200 does not seem right, since the items
+        // were not added to the list. This should be fixed on API
+        // side and this validation should then be updated
+        .expect("status", 200)
+        .expect("json", "results.*", { success: false });
+    });
+
+    it("cannot be added with invalid field values", async () => {
+      await frisby
+        .post(`${apiUrl}/list/${listId}/items`, {
+          items: [
+            {
+              media_type: "invalid_type",
+              media_id: "invalid_media_id",
+            },
+          ],
+        })
+        // @FIXME Status code 200 does not seem right, since the items
+        // were not added to the list. This should be fixed on API
+        // side and this validation should then be updated
+        .expect("status", 200)
+        .expect("json", "results.*", { success: false });
+    });
+
+    it("can be added", async () => {
+      await frisby
+        .post(`${apiUrl}/list/${listId}/items`, { items })
+        .expect("status", 200)
+        .expect("json", { success: true, results: items })
+        .expect("json", "results.*", { success: true });
+
+      // Check, that the items are available via /list/{listId}/item_status
+      for (const item of items) {
+        await frisby
+          .get(
+            `${apiUrl}/list/${listId}/item_status?media_id=${item.media_id}&media_type=${item.media_type}`
+          )
+          .expect("status", 200)
+          .expect("json", item);
+      }
+
+      // Remap the items object structure for the /list/{listId} endpoint
+      const results = items.map((item) => ({
+        id: item.media_id,
+        media_type: item.media_type,
+      }));
+
+      // Check, that the items are available via /list/{listId}
+      await frisby
+        .get(`${apiUrl}/list/${listId}`)
+        .expect("status", 200)
+        .expect("json", { results, total_results: 2 });
+    });
+
+    it("cannot be updat non-existent item", async () => {
+      await frisby
+        .put(`${apiUrl}/list/${listId}/items`, {
+          items: [
+            {
+              media_id: 1,
+              media_type: "movie",
+              comment: "comment",
+            },
+          ],
+        })
+        // @FIXME Status code 200 does not seem right, since the items
+        // were not update in the list. This should be fixed on API
+        // side and this validation should then be updated
+        .expect("status", 200)
+        .expect("json", "results.*", { success: false });
+    });
+
+    it("cannot update item with invalid values", async () => {
+      await frisby
+        .put(`${apiUrl}/list/${listId}/items`, {
+          items: [
+            {
+              ...items[0],
+              comment: 1,
+            },
+          ],
+        })
+        // @FIXME API should not return internal error, once
+        // fixed on API side, we should update this assertion
+        // to check the validation error message
+        .expect("status", 500)
+        .expect("json", { success: false });
+    });
+
+    it("can be updated", async () => {
+      // @FIXME Comment has to be for some reason unique,
+      // otherwise api returns status 500
+      const comment = `Hello, this is cool. (unique: ${Date.now()})`;
+      const item = items[0];
+
+      // Update the list item
+      await frisby
+        .put(`${apiUrl}/list/${listId}/items`, {
+          items: [{ ...item, comment }],
+        })
+        .expect("status", 200)
+        .expect("json", {
+          success: true,
+          results: [
+            {
+              ...items[0],
+              success: true,
+            },
+          ],
         });
 
-        it('cannot be created', () => {
-            return frisby
-                .post('https://api.themoviedb.org/4/list', {
-                    name: 'My Cool List',
-                    iso_639_1: 'en'
-                })
-                .expect('status', 401);
+      // Check the updated list item
+      await frisby
+        .get(`${apiUrl}/list/${listId}`)
+        .expect("status", 200)
+        .expect("json", {
+          comments: {
+            [`${item.media_type}:${item.media_id}`]: comment,
+          },
         });
     });
 
-    describe('with write access', () => {
-        let listId;
-
-        beforeAll(() => {
-            return Auth.setWriteAccess();
-        });
-
-        afterAll(async () => {
-            await frisby
-                .delete(`${apiUrl}/list/${listId}`)
-                .expect('status', 200)
-                .expect('json', { success: true });
-            
-            await frisby
-                .get(`${apiUrl}/list/${listId}`)
-                .expect('status', 404);
-        });
-
-        it('can be created with required fields only', async () => {
-            const postValues = {
-                name: 'My Cool List',
-                iso_639_1: 'en'
-            };
-            const response = await frisby
-                .post(`${apiUrl}/list`, postValues)
-                .expect('status', 201)
-                .expect('jsonTypes', { id: Joi.number().required() })
-                .expect('json', { success: true });
-
-            listId = response.json.id;
-
-            await frisby
-                .get(`${apiUrl}/list/${listId}`)
-                .expect('status', 200)
-                .expect('json', postValues);
-        });
-
-        it('can be updated', async () => {
-            const updateValues = {
-                name: 'My Cool Updated List',
-                description: 'Cool Description',
-                public: true
-            };
-
-            await frisby
-                .put(`${apiUrl}/list/${listId}`, updateValues)
-                .expect('status', 201)
-                .expect('json', { success: true });
-
-            await frisby
-                .get(`${apiUrl}/list/${listId}`)
-                .expect('status', 200)
-                .expect('json', updateValues);
-        });
-
-        describe('items', () => {
-            const items = [
-                {
-                    media_id: 652,
-                    media_type: 'movie'
-                },
-                {
-                    media_id: 60573,
-                    media_type: 'tv'
-                }
-            ];
-
-            it('can be added', async () => {
-                await frisby
-                    .post(`${apiUrl}/list/${listId}/items`, { items })
-                    .expect('status', 200)
-                    .expect('json', { success: true, results: items })
-                    .expect('json', 'results.*', { success: true });
-
-                for (let item of items) {
-                    await frisby
-                        .get(`${apiUrl}/list/${listId}/item_status?media_id=${item.media_id}&media_type=${item.media_type}`)
-                        .expect('status', 200)
-                        .expect('json', item);
-                }
-
-                const results = items.map(item => ({ id: item.media_id, media_type: item.media_type }));
-
-                await frisby
-                    .get(`${apiUrl}/list/${listId}`)
-                    .expect('status', 200)
-                    .expect('json', { results, total_results: 2 });
-            });
-
-            it('can be updated', async () => {
-                // Comment has to be for some reason unique,
-                // otherwise api returns status 500
-                const comment = `Hello, this is cool. (unique: ${Date.now()})`;
-                const item = items[0];
-
-                await frisby
-                    .put(`${apiUrl}/list/${listId}/items`, { items: [{ ...item, comment }]})
-                    .expect('status', 200)
-                    .expect('json', { success: true, results: [{
-                        ...items[0],
-                        success: true
-                    }]});
-
-                await frisby
-                    .get(`${apiUrl}/list/${listId}`)
-                    .expect('status', 200)
-                    .expect('json', { comments: { [`${item.media_type}:${item.media_id}`]: comment }});
-            });
-
-            it('can be removed', async () => {
-                const item = items[0];
-
-                await frisby
-                    .delete(`${apiUrl}/list/${listId}/items`, { items: [item]})
-                    .expect('status', 200)
-                    .expect('json', { success: true });
-
-                await frisby
-                    .get(`${apiUrl}/list/${listId}/item_status?media_id=${item.media_id}&media_type=${item.media_type}`)
-                    .expect('status', 404);
-            });
-
-            it('can be cleared', async () => {
-                const item = items[1];
-
-                await frisby
-                    .get(`${apiUrl}/list/${listId}/clear`)
-                    .expect('status', 200)
-                    .expect('json', {
-                        items_deleted: 1,
-                        success: true
-                    });
-
-                await frisby
-                    .get(`${apiUrl}/list/${listId}/item_status?media_id=${item.media_id}&media_type=${item.media_type}`)
-                    .expect('status', 404);
-            });
-        });
+    it("cannot remove non-existent item", async () => {
+      await frisby
+        .delete(`${apiUrl}/list/${listId}/items`, {
+          items: [
+            {
+              media_type: "tv",
+              media_id: 1,
+            },
+          ],
+        })
+        // @FIXME Status code 200 does not seem right, since no items
+        // were removed from the list. This should be fixed on API
+        // side and this validation should then be updated
+        .expect("status", 200)
+        .expect("json", "results.*", { success: false });
     });
+
+    it("can be removed", async () => {
+      const item = items[0];
+
+      // Delete the list item
+      await frisby
+        .delete(`${apiUrl}/list/${listId}/items`, { items: [item] })
+        .expect("status", 200)
+        .expect("json", { success: true });
+
+      // Check, that the deleted list item no longer exists
+      await frisby
+        .get(
+          `${apiUrl}/list/${listId}/item_status?media_id=${item.media_id}&media_type=${item.media_type}`
+        )
+        .expect("status", 404);
+    });
+
+    it("can be cleared", async () => {
+      const { media_id, media_type } = items[1];
+
+      // Clear all the remaining list items
+      await frisby
+        .get(`${apiUrl}/list/${listId}/clear`)
+        .expect("status", 200)
+        .expect("json", {
+          items_deleted: 1,
+          success: true,
+        });
+
+      // Check, tha the remaingin list item no longer exists
+      await frisby
+        .get(
+          `${apiUrl}/list/${listId}/item_status?media_id=${media_id}&media_type=${media_type}`
+        )
+        .expect("status", 404);
+    });
+  });
 });
--- fixtures/auth.js
+++ fixtures/auth.js
@@ -1,131 +1,133 @@
-import frisby from 'frisby';
-import { apiUrl, username, password, readAccessToken } from '../config';
+import frisby from "frisby";
+import { apiUrl, username, password, readAccessToken } from "../config";
 
 /**
  * This class provides authorization methods for TMDB API
  */
 export default class Auth {
-    /**
-     * Configurates Authorization header with read access token
-     */
-    static setReadAccess() {
-        frisby.globalSetup({
-            request: {
-                headers: {
-                    'Authorization': `Bearer ${readAccessToken}`
-                }
-            }
-        });
-    }
+  /**
+   * Configurates Authorization header with read access token
+   */
+  static setReadAccess() {
+    frisby.globalSetup({
+      request: {
+        headers: {
+          Authorization: `Bearer ${readAccessToken}`,
+        },
+      },
+    });
+  }
 
-    /**
-     * Configurates Authorization header with write access token.
-     * 
-     * It does following steps to get the token:
-     *  - login to TMDB website to get authorized session cookie
-     *  - generate request token
-     *  - approve the generated token using the authorized session cookie
-     *  - generate the write access token
-     * The token is then set as default Authorization header in frisbe configuration
-     * @returns {Promise}
-     */
-    static async setWriteAccess() {
-        const cookie = await this._getAuthorizedCookie();
-        const requestToken = await this._getRequestToken();
+  /**
+   * Configurates Authorization header with write access token.
+   *
+   * It does following steps to get the token:
+   *  - login to TMDB website to get authorized session cookie
+   *  - generate request token
+   *  - approve the generated token using the authorized session cookie
+   *  - generate the write access token
+   * The token is then set as default Authorization header in frisby configuration
+   * @returns {Promise}
+   */
+  static async setWriteAccess() {
+    const cookie = await this._getAuthorizedCookie();
+    const requestToken = await this._getRequestToken();
 
-        await this._approveRequestToken(requestToken, cookie);
+    await this._approveRequestToken(requestToken, cookie);
 
-        const writeAccessToken = await this._getWriteAccessToken(requestToken);
+    const writeAccessToken = await this._getWriteAccessToken(requestToken);
 
-        frisby.globalSetup({
-            request: {
-                headers: {
-                    'Authorization': `Bearer ${writeAccessToken}`
-                }
-            }
-        });
-    }
+    frisby.globalSetup({
+      request: {
+        headers: {
+          Authorization: `Bearer ${writeAccessToken}`,
+        },
+      },
+    });
+  }
 
-    /**
-     * Generates an authorized session cookie.
-     * This can be used to approve API request_token without any manual action.
-     * 
-     * Following steps are performed:
-     *  - get an unauthorized cookie from tmdb website (neccessary for login request to work properly)
-     *  - send login request to generate authorized cookie
-     * @returns {Promise<String>} authorized session cookie
-     */
-    static async _getAuthorizedCookie() {
-        const cookie = await this._getUnauthorizedCookie();
-        const response = await frisby.fetch('https://www.themoviedb.org/login', {
-            redirect: 'manual', // Do not follow redirects!
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                cookie
-            },
-            body: `username=${username}&password=${password}`,
-            method: 'POST'
-        });
-        
-        return this._getTmdbSessionCookieFromResponse(response);
-    }
+  /**
+   * Generates an authorized session cookie.
+   * This can be used to approve API request_token without any manual action.
+   *
+   * Following steps are performed:
+   *  - get an unauthorized cookie from tmdb website (neccessary for login request to work properly)
+   *  - send login request to generate authorized cookie
+   * @returns {Promise<String>} authorized session cookie
+   */
+  static async _getAuthorizedCookie() {
+    const cookie = await this._getUnauthorizedCookie();
+    const response = await frisby.fetch("https://www.themoviedb.org/login", {
+      redirect: "manual", // Do not follow redirects!
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        cookie,
+      },
+      body: `username=${username}&password=${password}`,
+      method: "POST",
+    });
 
-    /**
-     * Generates an unauthorized session cookie.
-     * @returns {Promise<String>} unauthorized session cookie
-     */
-    static async _getUnauthorizedCookie() {
-        const response = await frisby.get('https://www.themoviedb.org/login');
+    return this._getTmdbSessionCookieFromResponse(response);
+  }
 
-        return this._getTmdbSessionCookieFromResponse(response);
-    }
+  /**
+   * Generates an unauthorized session cookie.
+   * @returns {Promise<String>} unauthorized session cookie
+   */
+  static async _getUnauthorizedCookie() {
+    const response = await frisby.get("https://www.themoviedb.org/login");
 
-    /**
-     * Extracts `tmdb.session` cookie from `set-cookie` header
-     * @param {frisby.FrisbyResponse} response
-     * @returns {String} `tmdb.session` cookie
-     */
-    static _getTmdbSessionCookieFromResponse(response) {
-        return response.headers.raw()['set-cookie'].find(c => c.startsWith('tmdb.session'));
-    }
+    return this._getTmdbSessionCookieFromResponse(response);
+  }
 
-    /**
-     * Generates TMDB API auth request token
-     * @returns {Promise<String>} request token
-     */
-    static async _getRequestToken() {
-        const response = await frisby.post(`${apiUrl}/auth/request_token`);
+  /**
+   * Extracts `tmdb.session` cookie from `set-cookie` header
+   * @param {frisby.FrisbyResponse} response
+   * @returns {String} `tmdb.session` cookie
+   */
+  static _getTmdbSessionCookieFromResponse(response) {
+    return response.headers
+      .raw()
+      ["set-cookie"].find((c) => c.startsWith("tmdb.session"));
+  }
 
-        return response.json.request_token;
-    }
+  /**
+   * Generates TMDB API auth request token
+   * @returns {Promise<String>} request token
+   */
+  static async _getRequestToken() {
+    const response = await frisby.post(`${apiUrl}/auth/request_token`);
 
-    /**
-     * Approves provided request token using the provided authorized cookie
-     * @param {String} token request token
-     * @param {String} cookie authorized cookie
-     * @returns {Promise<frisby.FrisbyResponse>}
-     */
-    static _approveRequestToken(token, cookie) {
-        return frisby.fetch('https://www.themoviedb.org/auth/access/approve', {
-            headers: {
-              'content-type': 'application/x-www-form-urlencoded',
-              cookie
-            },
-            body: `request_token=${token}&time=${Date.now() / 1000}&submit=Approve`,
-            method: 'POST'
-        });
-    }
+    return response.json.request_token;
+  }
 
-    /**
-     * Generates write access token for the approved request token
-     * @param {String} requestToken approved request token
-     * @returns {Promise<String>} write access token
-     */
-    static async _getWriteAccessToken(requestToken) {
-        const response = await frisby.post(`${apiUrl}/auth/access_token`, {
-            request_token: requestToken
-        });
+  /**
+   * Approves provided request token using the provided authorized cookie
+   * @param {String} token request token
+   * @param {String} cookie authorized cookie
+   * @returns {Promise<frisby.FrisbyResponse>}
+   */
+  static _approveRequestToken(token, cookie) {
+    return frisby.fetch("https://www.themoviedb.org/auth/access/approve", {
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        cookie,
+      },
+      body: `request_token=${token}&time=${Date.now() / 1000}&submit=Approve`,
+      method: "POST",
+    });
+  }
 
-        return response.json.access_token;
-    }
+  /**
+   * Generates write access token for the approved request token
+   * @param {String} requestToken approved request token
+   * @returns {Promise<String>} write access token
+   */
+  static async _getWriteAccessToken(requestToken) {
+    const response = await frisby.post(`${apiUrl}/auth/access_token`, {
+      request_token: requestToken,
+    });
+
+    return response.json.access_token;
+  }
 }
